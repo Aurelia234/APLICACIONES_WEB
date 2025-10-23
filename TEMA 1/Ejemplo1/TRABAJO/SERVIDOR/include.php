1141c0327906ff7f41a84b91c1479897da7abc28
Aurelia234 <ams0096@alu.medac.es>
2025-10-23 19:43:29 +0200

    servidor

--- TEMA 1/Ejemplo1/TRABAJO/SERVIDOR/include.php
+++ TEMA 1/Ejemplo1/TRABAJO/SERVIDOR/include.php
@@ -1,13 +1,13 @@
 <?php
-$nombre= $_get['nombre'];
-$apellido= $_get['apellido'];
-$edad= $_get['edad'];
+$nombre= ($_GET["nombre"]);
+$apellido= ($_GET["apellido"]);
+$edad= intval ($_GET["edad"]);
 
 function comprobarEdad ($nombre, $apellido, $edad) {
     if ($edad >= 18) {
-       echo "Hola, $nombre $apellido, eres mayor de edad " ;;
+       echo "Hola, $nombre $apellido, eres mayor de edad ";
     } else {
-         echo "Hola, $nombre $apellido, eres menor de edad " ;;
+         echo "Hola, $nombre $apellido, eres menor de edad ";
     }
 }
 //llamamos a la funcion
