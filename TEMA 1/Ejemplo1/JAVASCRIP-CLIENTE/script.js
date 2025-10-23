ec1c2679232b0f54705bfb63d7e7301c3c66bf58
Aurelia234 <ams0096@alu.medac.es>
2025-10-23 17:10:53 +0200

    cliente

--- TEMA 1/Ejemplo1/JAVASCRIP-CLIENTE/script.js
+++
@@ -1,8 +0,0 @@
-//Document write (Actividad crear JavaScript Cliente)
-var parrafo = document.createElement("p");
-parrafo.innerHTML = "Hola, este es un párrafo creado con JavaScript.";
-document.body.appendChild(parrafo);
-parrafo.style.color = "blue";
-parrafo.style.fontSize = "20px";
-
-
